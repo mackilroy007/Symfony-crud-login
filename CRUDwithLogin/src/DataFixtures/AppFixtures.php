e50e6931a0d4ad7c4b55a5ab667ac2d4e1a2a9ef
mackilroy007 <mackilroy007@gmail.com>
2020-12-04 13:30:06 +0100

    generating a fixture class and inserting password and email encoder

--- CRUDwithLogin/src/DataFixtures/AppFixtures.php
+++ CRUDwithLogin/src/DataFixtures/AppFixtures.php
@@ -2,15 +2,46 @@
 
 namespace App\DataFixtures;
 
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+use App\entity\User;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
+
+use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
+
 {
-    public function load(ObjectManager $manager)
+
+    private $passwordEncoder;
+
+
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+
+    {
+
+        $user = new User();
+
+        $user->setPassword($this->passwordEncoder->encodePassword(
+
+            $user,
+
+            '123123'
+
+        ));
+
+        $user->setEmail("user@gmail.com");
+
+        $manager->persist($user);
 
         $manager->flush();
     }
